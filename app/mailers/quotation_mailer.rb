c1f80ce4afb374447629e5fae3431f45302cebc4
Dan Cornwell <dtcornwell@live.com>
2013-11-25 00:22:13 +0000

    updated mailer

--- app/mailers/quotation_mailer.rb
+++ app/mailers/quotation_mailer.rb
@@ -1,8 +1,10 @@
 class QuotationMailer < ActionMailer::Base
 
+    default from: "no-reply@dtcinsuranceunderwriter.com"
+
      def send_code(quotation)
        @quotation = quotation
-       mail(:to => quotation.person.email, :subject => "DTC Underwriter - Your quotation code", :from => "dtcunderwriter@gmail.com")
+       mail(:to => quotation.person.email, :subject => "DTC Underwriter - Your quotation code")
      end
 
 end
--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "gmail.com",
-    :user_name            => "dtcunderwriter",
-    :password             => "overwriter",
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-}
