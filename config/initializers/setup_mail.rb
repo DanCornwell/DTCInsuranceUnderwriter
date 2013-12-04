229d849e5dcd71f12836d211b709ef68f5b1dc1e
Dan Cornwell <dtcornwell@live.com>
2013-12-04 17:27:53 +0000

    changed mail domain to match new domain

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
 ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "protected-bastion-3103.herokuapp.com",
+    :domain               => "dtcinsuranceunderwriter.herokuapp.com",
     :user_name            => "dtcinsuranceunderwriter",
     :password             => ENV["PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "protected-bastion-3103.herokuapp.com"
+ActionMailer::Base.default_url_options[:host] = "dtcinsuranceunderwriter.herokuapp.com"
